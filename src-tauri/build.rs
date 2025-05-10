9b6ee7708992ea229734c5175be978ae11dee0b5
dafen173 <dafen@ukr.net>
2025-05-10 14:23:41 +0300

    Fix Resource Generation

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,15 @@
+// fn main() {
+//     tauri_build::build()
+// }
+
+
 fn main() {
-    tauri_build::build()
+    // Disable default resource generation
+    println!("cargo:rerun-if-changed=build.rs");
+    
+    // Only enable on Windows native builds
+    if std::env::var("TARGET").unwrap().contains("windows") {
+        tauri_build::build();
+    }
 }
+
