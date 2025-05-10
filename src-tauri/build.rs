248445076b888f97bc949e8964f36a13fc60323f
dafen173 <dafen@ukr.net>
2025-05-10 15:08:09 +0300

    Fix the Root Cause

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,15 +1,8 @@
-// fn main() {
-//     tauri_build::build()
-// }
-
-
 fn main() {
-    // Disable default resource generation
-    println!("cargo:rerun-if-changed=build.rs");
-    
-    // Only enable on Windows native builds
+    // Only enable resource generation on native Windows builds
     if std::env::var("TARGET").unwrap().contains("windows") {
         tauri_build::build();
+    } else {
+        println!("cargo:rustc-link-arg=-Wl,--allow-multiple-definition");
     }
 }
-
