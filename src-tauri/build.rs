9c199750bc55b7b5e3f70feb044ee5a70005cb07
dafen173 <dafen@ukr.net>
2025-05-13 14:58:21 +0300

    edit version of tauri

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,8 +1,3 @@
 fn main() {
-    // Only enable resource generation on native Windows builds
-    if std::env::var("TARGET").unwrap().contains("windows") {
-        tauri_build::build();
-    } else {
-        println!("cargo:rustc-link-arg=-Wl,--allow-multiple-definition");
-    }
+  tauri_build::build()
 }
